fae0f9506feb04748e5673668932a3b877e9fe84
David Wilder <dwilder@us.ibm.com>
2020-01-02 14:47:43 -0800

    update the segfaut program to fault

--- ci/segfault-gdb-demo.sh
+++ ci/segfault-gdb-demo.sh
@@ -5,17 +5,16 @@ set -ex
 cat >testprg.c <<EOS
 int main(int argc, char *argv[])
 {
-
-        char b;
         char *a;
-	b=a[10];
-	return 0;
+        a=(char *)0;
+        a[1]="A";
+        return 0;
 }
 EOS
 
 cat testprg.c
 # ${CC:-cc} -Wall -o testprg testprg.c
-${CC:-cc} -o testprg testprg.c
+${CC:-cc} -g -o testprg testprg.c
 
 gdb ./testprg
 
