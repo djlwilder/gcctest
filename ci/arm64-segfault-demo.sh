9ed9fb0c0434cdbfe8b0e8ebdf5f67b3a3058bb6
David Wilder <dwilder@us.ibm.com>
2020-01-03 13:36:09 -0800

    remove the unlink to verify

--- ci/arm64-segfault-demo.sh
+++ ci/arm64-segfault-demo.sh
@@ -2,21 +2,6 @@
 
 set -ex
 
-# cat >testprg.c <<EOS
-# #include <stdio.h>
-# int main(int argc, char *argv[])
-# {
-# 	int c = 'z';
-# 	c = getc(stdin);
-# 	if (c == EOF)
-# 		printf("EOF!\n");
-# 	else
-# 		printf("got '%c' (0x%x)\n", c, c);
-# 	return 0;
-# }
-# EOS
-
-
 # cat > ~root/.gdbinit <<EOS
 # set pagination off
 # set logging file gdb.output
@@ -41,8 +26,8 @@ int main(int argc, char *argv[])
 	int fd;
 
 	fd=open("./fifo",O_RDWR);
-	fstat(fd, &st);
-	unlink("./fifo");
+	// The fstat segfaults if the named pipe has been unlinked.
+	# unlink("./fifo");
 	fstat(fd, &st);
 	return 0;
 }
@@ -51,19 +36,8 @@ EOS
 cat testprg.c
 ${CC:-cc} -Wall -g -o testprg testprg.c
 
-# ./testprg </dev/null
-# echo a >input
-# ./testprg <input
-# echo b |./testprg
-
 rm -f fifo
 mkfifo fifo
-# echo e > ./fifo
-
-# exec 8<>fifo
-# echo d >fifo
-# ls -l /proc/self/fd
-# ./testprg <&8
 
 # This will segfault on arm64 and ppc64le, but not elsewhere:
 ls -l /proc/self/fd
